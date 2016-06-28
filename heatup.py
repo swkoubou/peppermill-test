0ac81915d5ee817ea7935cd4550907e8cef69286
uryoya <uryoya.ur@gmail.com>
2016-06-28 21:45:31 +0900

    heatupAPIの、ダミーの/analyzeエンドポイントを作った

---
+++ heatup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+heatup API
+
+USAGE:
+Run the following command in your terminal.
+$ exprot FLASK_APP=heatup.py
+$ flask run
+"""
+from flask import Flask, request
+import json
+
+from random import randrange
+app = Flask(__name__)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    response = {}
+    if randrange(1, 3) == 1:
+        response['is_burst'] = True
+    else:
+        response['is_burst'] = False
+    return json.dumps(response)
