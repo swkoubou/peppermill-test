f7f7a513458e5d827a4d98c27a19cef211449410
snakazawa <nakazawa.tms0314@gmail.com>
2016-07-13 20:47:46 +0900

    一応timelineでは動いたけど、サンプル参考にしてもアプリから動作しない。

--- heatup-animation/app/src/main/java/org/swkoubou/peppermill/meetingsupport/heataup_animation/Animations.java
+++ heatup-animation/app/src/main/java/org/swkoubou/peppermill/meetingsupport/heataup_animation/Animations.java
@@ -1,38 +1,10 @@
 package org.swkoubou.peppermill.meetingsupport.heataup_animation;
 
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.aldebaran.qi.QiCallback;
-import com.aldebaran.qi.sdk.Qi;
-import com.aldebaran.qi.sdk.object.interaction.Say;
+import android.content.Context;
+import com.aldebaran.qi.sdk.object.actuation.Animation;
 
 public class Animations {
-    private static final String TAG = "Animations";
-
-    private AppCompatActivity activity;
-
-    Animations (AppCompatActivity activity) {
-        this.activity = activity;
-    }
-
-    public void Say () {
-        Say say = new Say(this.activity);
-        say.run("Hello, world!").then(Qi.onUiThread(new QiCallback<Void>() {
-            @Override
-            public void onResult(Void ignore) {
-                Log.d(TAG, "result on thread " + Thread.currentThread().getName());
-            }
-
-            @Override
-            public void onError(Throwable error) {
-                Log.e(TAG, "error", error);
-            }
-
-            @Override
-            public void onCancel() {
-                Log.w(TAG, "cancel");
-            }
-        }));
+    static public Animation HeatUp(Context context) {
+        return Animation.fromResources(context, R.raw.heatup);
     }
 }
--- heatup-animation/app/src/main/java/org/swkoubou/peppermill/meetingsupport/heataup_animation/MainActivity.java
+++ heatup-animation/app/src/main/java/org/swkoubou/peppermill/meetingsupport/heataup_animation/MainActivity.java
@@ -2,22 +2,25 @@ package org.swkoubou.peppermill.meetingsupport.heataup_animation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import com.aldebaran.qi.QiCallback;
-import com.aldebaran.qi.sdk.Qi;
-import com.aldebaran.qi.sdk.object.interaction.Say;
+
+import com.aldebaran.qi.sdk.object.actuation.Animate;
+import com.aldebaran.qi.sdk.object.actuation.Animation;
 
 
+// ref. https://android.aldebaran.com/doc/elephant_tutorial.html#elephant-tutorial
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "SayActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Animations animation = new Animations(this);
-        animation.Say();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Animation heatUpAnimation = Animations.HeatUp(this);
+        Animate animate = new Animate(this);
+        animate.run(heatUpAnimation);
+    }
 }
