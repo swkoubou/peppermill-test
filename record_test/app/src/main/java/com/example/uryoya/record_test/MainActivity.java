cfd3be1b43d38437e5934b591512cabedeb6957d
uryoya <uryoya.ur@gmail.com>
2016-07-23 15:55:50 +0900

    Recorderに移植して要らなくなった部分を消した

--- record_test/app/src/main/java/com/example/uryoya/record_test/MainActivity.java
+++ record_test/app/src/main/java/com/example/uryoya/record_test/MainActivity.java
@@ -3,17 +3,12 @@ package com.example.uryoya.record_test;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import java.io.IOException;
-import java.io.StringReader;
 
 public class MainActivity extends AppCompatActivity {
-    private Handler _handler = new Handler();
-    private Runnable stopRecordAfter5sec;
-
     private MediaPlayer   mPlayer = null;
     private MediaRecorder mRecorder = null;
     private static String mFileName = null;
@@ -25,21 +20,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d(">>>", mFileName);
     }
 
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }
-
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
 
     private void startPlaying() {
         mPlayer = new MediaPlayer();
@@ -55,33 +35,6 @@ public class MainActivity extends AppCompatActivity {
     private void stopPlaying() {
         mPlayer.release();
         mPlayer = null;
-    }
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-            Log.e(LOG_TAG, e.getMessage());
-        }
-
-        try {
-            mRecorder.start();
-        } catch (IllegalStateException e) {
-            Log.e(LOG_TAG, e.getMessage());
-        }
-    }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
     }
 
     @Override
