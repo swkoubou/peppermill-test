51c562f73a9ced13f88ace86abe96faebe776bdf
watayuz <w-desu@i.softbank.jp>
2016-07-24 01:46:58 +0900

    ffmpegを使って3gpをwavに変換できるようにし、handlerを実装した

--- Convert3gpToWAV/app/src/main/java/com/myown/convert3gptowav/Convert3gpToWAV.java
+++ Convert3gpToWAV/app/src/main/java/com/myown/convert3gptowav/Convert3gpToWAV.java
@@ -2,6 +2,7 @@ package com.myown.convert3gptowav;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.os.Handler;
 import android.util.Log;
 
 import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
@@ -21,56 +22,91 @@ import java.io.PrintStream;
  * Created by Yuta.Watanabe on 2016/07/23.
  */
 public class Convert3gpToWAV {
-	public void convert(Context context){
-		AssetManager am = context.getAssets();
-		InputStream is = null;
-		try {
-			is = am.open("audiorecordtest.3gp");
-		}catch (IOException e){
-			e.printStackTrace();
-		}
-		File file = createFileFromInputStream(is);
+	private Context context = null;
+	private FFmpeg ffmpeg = null;
+	private String TAG = this.getClass().toString();
 
-		String fpstr = file.getAbsolutePath();
-		String outname = "/data/data/" + context.getPackageName() + "/audiorecordtest.wav";
-		convert(fpstr, outname);
-//		String exeQuery = "ffmpeg -i "+fpstr+" -acodec pcm_u8 "+outname;
-//		try {
-//			Runtime.getRuntime().exec(exeQuery);
-//		}catch (IOException e){
-//			e.printStackTrace();
-//		}
+
+	public Convert3gpToWAV(Context context){
+		this.context = context;
+		ffmpeg = FFmpeg.getInstance(context);
+
 	}
+	public void convert(String input_file_path, String outputfile_path, int seconds){
 
-	public void convert(String fileabsolutepath, String outname){
-		String exeQuery = "ffmpeg -i "+fileabsolutepath+" -acodec pcm_u8 "+outname;
-		try {
-			Runtime.getRuntime().exec(exeQuery);
-		}catch (IOException e){
-			e.printStackTrace();
-		}
-	}
+		Handler handler = new Handler();
 
-	private File createFileFromInputStream(InputStream inputStream) {
-		try{
-			File f = new File("/data/data/com.myown.convert3gptowav/audiorecordtest.wav");
-			OutputStream outputStream = new FileOutputStream(f);
-			byte buffer[] = new byte[1024];
-			int length = 0;
+		String str = "-i " + input_file_path + " " + outputfile_path;
+		final String[] cmd = str.split(" ");
 
-			while((length=inputStream.read(buffer)) > 0) {
-				outputStream.write(buffer,0,length);
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				loadToExecute(cmd);
 			}
+		}, seconds * 1000);
+	}
 
-			outputStream.close();
-			inputStream.close();
+	private void loadToExecute(final String[] cmd){
+		try {
+			ffmpeg.loadBinary(new LoadBinaryResponseHandler() {
 
-			return f;
-		}catch (IOException e) {
-			//Logging exception
+				@Override
+				public void onStart() {
+					Log.d(TAG, "load onstart");
+				}
+
+				@Override
+				public void onFailure() {
+					Log.d(TAG, "load onFailure");
+				}
+
+				@Override
+				public void onSuccess() {
+					Log.d(TAG, "load onSuccess");
+				}
+
+				@Override
+				public void onFinish() {
+					Log.d(TAG, "load onFinish");
+					execute(cmd);
+				}
+			});
+		} catch (FFmpegNotSupportedException e) {
 			e.printStackTrace();
 		}
+	}
+	private void execute(String[] cmd){
+		try {
+			ffmpeg.execute(cmd, new ExecuteBinaryResponseHandler() {
 
-		return null;
+				@Override
+				public void onStart() {
+					Log.d(TAG, "execute onStart");
+				}
+
+				@Override
+				public void onProgress(String message) {
+					Log.d(TAG, message);
+				}
+
+				@Override
+				public void onFailure(String message) {
+					Log.d(TAG, message);
+				}
+
+				@Override
+				public void onSuccess(String message) {
+					Log.d(TAG, message);
+				}
+
+				@Override
+				public void onFinish() {
+					Log.d(TAG, "execute onFinish");
+				}
+			});
+		} catch (FFmpegCommandAlreadyRunningException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- Convert3gpToWAV/app/src/main/java/com/myown/convert3gptowav/MainActivity.java
+++ Convert3gpToWAV/app/src/main/java/com/myown/convert3gptowav/MainActivity.java
@@ -1,7 +1,17 @@
 package com.myown.convert3gptowav;
 
+import android.app.Application;
+import android.content.res.AssetManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Convert3gpToWAV convert3gpToWAV = new Convert3gpToWAV();
-		convert3gpToWAV.convert(MainActivity.this);
+		String in_filePath = "/data/data/com.myown.convert3gptowav/audiorecordtest.3gp";
+		String out_filePath = "/data/data/com.myown.convert3gptowav/audiorecordtest.wav";
+		Convert3gpToWAV convert3gpToWAV= new Convert3gpToWAV(MainActivity.this);
+		convert3gpToWAV.convert(in_filePath, out_filePath, 10);
 	}
+
 }
